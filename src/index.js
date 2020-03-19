f1e27870523f95d491ead5bd348c5be7c3e49606
dongyang.liu <dongyang.liu@cyberway.net.cn>
2020-03-19 20:06:57 +0800

    add

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'antd/dist/antd.css'
 import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 
-
+// AppContainer 
 //如果你想让你的应用离线运行，并且加载速度更快，你可以改变
 //取消注册()以注册()。注意，这有一些陷阱。
 //了解更多关于服务人员的信息:https://bit.ly/CRA-PWA
